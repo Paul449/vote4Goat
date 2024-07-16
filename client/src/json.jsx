0157400d94e1d96c10a2e5015228b2171372bc72
brackenluke <156158313+brackenluke@users.noreply.github.com>
2024-07-15 19:53:51 -0500

    fixed the leaderboard and voting

--- client/src/json.jsx
+++ client/src/json.jsx
@@ -2,12 +2,12 @@ export const json = {
     "elements": [
       {
         "type": "dropdown",
-        "name": "car",
-        "title": "Which is the brand of your car?",
+        "name": "Favorite Player",
+        "title": "Select your GOAT below",
         "isRequired": true,
         "showNoneItem": true,
         "showOtherItem": true,
-        "choices": [ "Ford", "Vauxhall", "Volkswagen", "Nissan", "Audi", "Mercedes-Benz", "BMW", "Peugeot", "Toyota", "Citroen" ]
+        "choices": [ "Michael Jordan", "LeBron James", "Kareem Abdul-Jabbar", "Hakeem Olajumon", "Shaquille ONeal", "Kobe Bryant", "Larry Bird", "Tim Duncan", "Wilt Chamberlain", "Magic Johnson" ]
       }
     ],
     "showQuestionNumbers": false,
